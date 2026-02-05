cee6c5d8b8df7f73b1f096f1e508a65490a6e998
Andres <andresedmarquez107@gmail.com>
2026-02-05 18:56:35 -0400

    quitar cors

--- backend/server.py
+++ backend/server.py
@@ -43,10 +43,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://appweb-aquavalle-70xn902ig-andres-marquezs-projects-8503559d.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
